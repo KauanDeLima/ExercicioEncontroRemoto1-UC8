0794affd6713c471532f6cdbf90e1f1ad35c9cdd
Kauan <Kauancrdl3@gmail.com>
2022-04-12 19:44:53 -0300

    exercicio

---
+++ encontroRemoto1.js
@@ -0,0 +1,20 @@
+let listaPecas = ["Guidão", "Aro", "Selim", "Garfo","pastilha", "Freio", "Pneu", "Cambio", "Buzina", "Corrente"];
+
+let nomePeca = "aro";
+let pesoPeca = 450 ;
+
+    if(listaPecas.length >= 10){ 
+
+    console.log("Capacidade máxima atingido , limite de 10 peças ");
+
+                 } else{ 
+            if(pesoPeca >= 100){ 
+            console.log("Peso da peça superior a 100 gramas, pode cadastrar.");
+
+            if(nomePeca.length < 3){ 
+                    
+            console.log("alerta! O nome da peça deve ter mais que 3 caracteres.");
+
+    }
+  }
+}
